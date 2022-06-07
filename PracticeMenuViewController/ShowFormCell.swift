102a11332faf19226bcf13f243693510335b9c70
0102johnny <rt2824.recruit.1022@gmail.com>
2022-06-07 16:28:23 +0900

    showFormCellをTableViewに登録して表示する処理の実装

---
+++ PracticeMenuViewController/ShowFormCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShowFormCell.swift
+//  PracticeMenuViewController
+//
+//  Created by Johnny Toda on 2022/06/07.
+//
+
+import UIKit
+
+class ShowFormCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- PracticeMenuViewController/ViewController.swift
+++ PracticeMenuViewController/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let nib = "showFormCell"
+
+    @IBOutlet weak var menuTableView: UITableView! {
+        didSet {
+            menuTableView.register(UINib(nibName: nib, bundle: nil), forCellReuseIdentifier: "CellID")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        menuTableView.delegate = self
+        menuTableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        menuTableView.dequeueReusableCell(withIdentifier: "CellID", for: indexPath) as! ShowFormCell
+    }
+}
